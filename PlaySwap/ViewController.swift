0c0190d89992e2bb35ff76fd6e7588860521caa4
jwoody02 <jowo4949@colorado.edu>
2021-10-04 15:07:45 -0600

    added some basic functions

--- PlaySwap/ViewController.swift
+++ PlaySwap/ViewController.swift
@@ -13,7 +13,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
 
+extension UIViewController {
+    func createButton() -> UIButton {
+        let newButton = UIButton()
+        view.addSubview(newButton)
+        return newButton
+    }
+    func createView() -> UIView {
+        let newView = UIView()
+        view.addSubview(newView)
+        return newView
+    }
+    func createImage(named:String) -> UIImageView {
+        let newImage = UIImageView()
+        newImage.image = UIImage(named: named)
+        view.addSubview(newImage)
+        return newImage
+    }
+}
